d9a9c50e1d87d7091341538034aae1ff37a51f0e
shire <shiresese@gmail.com>
2015-06-11 02:04:51 +0900

    add scroll

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
+var scrollMargin = 50;
+
+var rows = new Array();
+
+var CRow = function(element, top){
+  this.top = top;
+  this.element = element;
+}
+
+function fixedScroll(){
+  var pos = window.scrollY;
+  rows.forEach(function(row){
+    if(pos < row.top - scrollMargin){
+      window.location = Config.basePath + "#" + $(row.element).attr("id");
+    }
+  })
+
+}
+
 $(function(){
+  var rowElements = document.getElementsByClassName("row");
+  var currentHeight = 0;
+  if(rowElements.length > 0){
+    for(var i = 0; i < rowElements.length; i++){
+      rows.push(new CRow(rowElements[i], currentHeight));
+      currentHeight += $(rowElements[i]).height();
+    }
+  }
+
+  window.onscroll = fixedScroll;
+
+  console.log(rows);
 
 });
