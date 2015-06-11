2934a9c28d81e0a17cdca9f51fce70662eacba01
shire <shiresese@gmail.com>
2015-06-11 19:58:19 +0900

    fixed hash

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 
 var currentPage = "srss";
 
+function initPos(){
+  if(window.location.hash === "") {
+    window.scrollTo($("#shire").width(), 0);
+  } else {
+    if(window.location.hash === "#shire") {
+      window.scrollTo(0, 0);
+    } else if(window.location.hash === "#shire") {
+      window.scrollTo($("#shire").width() + $("#srss").width(), 0);
+    }
+  }
+}
+
 $(function(){
   var scrollStopEvent = new $.Event("scrollstop");
   var delay = 200;
@@ -14,15 +26,6 @@ $(function(){
 
   $(window).scroll(scrollStopEventTrigger);
   $("body").on("touchmove", scrollStopEventTrigger);
-  if(window.location.hash === "") {
-    window.scrollTo($("#shire").width(), 0);
-  } else {
-    if(window.location.hash === "#shire") {
-      window.scrollTo(0, 0);
-    } else if(window.location.hash === "#shire") {
-      window.scrollTo($("#shire").width() + $("#srss").width(), 0);
-    }
-  }
 
   $(window).on("scrollstop", function(){
 
